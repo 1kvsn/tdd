e057456087d9ef6aad142e06c928a1c70bc7db3a
nick <narya@kyruushealth.com>
2024-12-17 11:00:29 +0530

    add remaining test cases

--- src/leap-year/checkLeapYear.js
+++ src/leap-year/checkLeapYear.js
@@ -1,9 +1,17 @@
 export function checkLeapYear(num) {
-  if (num < 0) {
-    throw new Error("Input must be a positive integer");
-  }
+	if (num < 0) {
+		throw new Error('Input must be a positive integer');
+	}
 
-  if (num % 4 !== 0) {
-    return "not a leap year";
-  }
+	if (num % 4 !== 0) {
+		return false;
+	}
+
+	if (num % 100 === 0 && num % 400 !== 0) {
+		return false;
+	}
+	if (num % 400 === 0) {
+		return true;
+	}
+	return true;
 }
--- src/leap-year/checkLeapYear.test.js
+++ src/leap-year/checkLeapYear.test.js
@@ -6,6 +6,18 @@ describe("check leap year", () => {
   });
 
   it("should not be a leap year if num not divisible by 4", () => {
-    expect(checkLeapYear(2001)).toBe("not a leap year");
+    expect(checkLeapYear(1997)).toBe(false);
+  });
+
+  it("should be a leap year if num divisible by 4", () => {
+    expect(checkLeapYear(1996)).toBe(true);
+  });
+
+  it("should be a leap year if num divisible by 400", () => {
+    expect(checkLeapYear(1600)).toBe(true);
+  })
+
+  it("should not be a leap year if num divisible by 100 but not by 400", () => {
+    expect(checkLeapYear(1800)).toBe(false);
   });
 });
